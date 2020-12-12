100799ce717a75782d00caf4d295e69d42f9075b
Srinivasan, Shankar (206464934) <Shankar.Srinivasan@nbcuni.com>
2020-12-12 14:37:50 -0500

    chaning the robo file to contrib folder

--- .circleci/RoboFile.php
+++ .circleci/RoboFile.php
@@ -193,7 +193,7 @@ class RoboFile extends \Robo\Tasks
             ->mkdir('artifacts/phpunit', 777);
         $tasks[] = $this->taskExecStack()
             ->dir('web')
-            ->exec('../vendor/bin/phpunit -c core --debug --verbose --log-junit ../artifacts/phpunit/phpunit.xml modules/custom');
+            ->exec('../vendor/bin/phpunit -c core --debug --verbose --log-junit ../artifacts/phpunit/phpunit.xml modules/contrib');
         return $tasks;
     }
 
@@ -213,7 +213,7 @@ class RoboFile extends \Robo\Tasks
             ->mkdir('artifacts/coverage-html', 777);
         $tasks[] = $this->taskExecStack()
             ->dir('web')
-            ->exec('../vendor/bin/phpunit -c core --debug --verbose --coverage-xml ../artifacts/coverage-xml --coverage-html ../artifacts/coverage-html modules/custom');
+            ->exec('../vendor/bin/phpunit -c core --debug --verbose --coverage-xml ../artifacts/coverage-xml --coverage-html ../artifacts/coverage-html modules/contrib');
         return $tasks;
     }
 
@@ -231,8 +231,8 @@ class RoboFile extends \Robo\Tasks
         $tasks[] = $this->taskFilesystemStack()
             ->mkdir('artifacts/phpcs');
         $tasks[] = $this->taskExecStack()
-            ->exec('vendor/bin/phpcs --standard=Drupal --report=junit --report-junit=artifacts/phpcs/phpcs.xml web/modules/custom')
-            ->exec('vendor/bin/phpcs --standard=DrupalPractice --report=junit --report-junit=artifacts/phpcs/phpcs.xml web/modules/custom');
+            ->exec('vendor/bin/phpcs --standard=Drupal --report=junit --report-junit=artifacts/phpcs/phpcs.xml web/modules/contrib')
+            ->exec('vendor/bin/phpcs --standard=DrupalPractice --report=junit --report-junit=artifacts/phpcs/phpcs.xml web/modules/contrib');
         return $tasks;
     }
 
